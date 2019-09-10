1ae0180fe58b59380f391d5a019051fe12c3435b
SharleneW <pokey.wen@gmail.com>
2019-09-11 01:29:11 +0800

    update

---
+++ js/all.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+  $("#sidebar").on("click", function() {
+    $(".navbar_nav").slideToggle();
+  });
+
+  $(".scrollTop").on("click", function(e) {
+    e.preventDefault();
+    var target = $(this).attr("href");
+    var targetPos = $(target).offset().top;
+    var windowWidth = $(window).width();
+    var currentWidth = windowWidth < 768 ? targetPos - 76 : targetPos - 150;
+    $("html, body").animate({ scrollTop: currentWidth }, 1000);
+  });
+
+  $(window).on("scroll", function() {
+    var scrollTop = $(this).scrollTop();
+    var windowHeight = $(this).height();
+    var windowWidth = $(this).width();
+    if (windowWidth >= 768 - 15) {
+      $(".navbar_nav").slideDown();
+    } else {
+      $(".navbar_nav").slideUp();
+    }
+
+    $(".scrollTop").each(function() {
+      var target = $(this).attr("href");
+      var targetPos = $(target).offset().top;
+      var targetHeight = $(target).outerHeight();
+      // console.log("target", target);
+      // console.log("targetPos", targetPos);
+      // console.log("scrollTop", scrollTop);
+      if (
+        targetPos - 150 <= scrollTop &&
+        targetPos + targetHeight > scrollTop
+      ) {
+        $(".scrollTop").removeClass("active");
+        $(this).addClass("active");
+      } else {
+        $(this).removeClass("active");
+      }
+    });
+
+    $(".animated").each(function() {
+      var thisPos = $(this).offset().top;
+      if (scrollTop + windowHeight >= thisPos) {
+        $(this).addClass("fadeIn");
+      }
+      // console.log("thisPos", thisPos);
+    });
+  });
+
+  $(window).resize(function() {
+    var windowWidth = $(this).width();
+    // console.log("windowWidth", windowWidth);
+    if (windowWidth >= 768 - 15) {
+      $(".navbar_nav").slideDown();
+    } else {
+      $(".navbar_nav").slideUp();
+    }
+  });
+
+  $("#p_demo1").on("click", function() {
+    $("#demo1").css("display", "block");
+  });
+  $(".p_btn_close").on("click", function() {
+    $("#demo1").css("display", "none");
+  });
+
+  $("#btn_Send").on("click", function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var msg = $("#message").val();
+
+    Email.send({
+      Host: "smtp.gmail.com",
+      Username: "pokey.wen",
+      Password: "s6248w0927",
+      To: "pokey.wen@gmail.com",
+      From: email,
+      Subject: name,
+      Body: msg
+    }).then(message => alert(message));
+  });
+});
